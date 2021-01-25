2c77f25425d7fc602ccb6f99e7ee499da15d737a
GiacomoLPirrone <giacomollpirrone@gmail.com>
2021-01-25 13:00:25 +1100

    Spring initalizer files added and configured

---
+++ src/test/java/com/songscorer/songscorer/SongScorerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songscorer.songscorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongScorerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
