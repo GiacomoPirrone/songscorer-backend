cb7c6c488e8f727bd393a6e42b27df0b84983386
GiacomoLPirrone <giacomollpirrone@gmail.com>
2021-01-28 18:30:05 +1100

    Basic model and repository setup

---
+++ src/main/java/com/songscorer/songscorer/model/Review.java
@@ -0,0 +1,39 @@
+package com.songscorer.songscorer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long reviewId;
+    @NotNull
+    private String spotifyAlbumUrl;
+    @Nullable
+    @Lob
+    private String reviewDescription;
+    @NotNull
+    private Integer rating;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private Instant createdDate;
+
+
+}
---
+++ src/main/java/com/songscorer/songscorer/repository/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.songscorer.songscorer.repository;
+
+import com.songscorer.songscorer.model.Review;
+import com.songscorer.songscorer.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findAllByUser(User user);
+}
