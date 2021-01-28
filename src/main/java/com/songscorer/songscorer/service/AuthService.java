15c6bc7b717c9034f0e0cce66028d455399db61b
GiacomoLPirrone <giacomollpirrone@gmail.com>
2021-01-28 21:42:09 +1100

    Setup of verification tokens which will be assigned to a user, this enables a user to be verified after the signup and click a verification link sent by an email

--- src/main/java/com/songscorer/songscorer/service/AuthService.java
+++ src/main/java/com/songscorer/songscorer/service/AuthService.java
@@ -2,18 +2,31 @@ package com.songscorer.songscorer.service;
 
 import com.songscorer.songscorer.dto.RegisterRequest;
 import com.songscorer.songscorer.model.UserAccount;
+import com.songscorer.songscorer.model.VerificationToken;
+import com.songscorer.songscorer.repository.UserAccountRepository;
+import com.songscorer.songscorer.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    /* No @Autowired here, it is better to use constructor injections
+     * over field injections. An Explanation can be found here: https://bit.ly/2NENAjz
+     */
 
+    private final PasswordEncoder passwordEncoder;
+    private final UserAccountRepository userAccountRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
     public void signup(RegisterRequest registerRequest) {
         UserAccount userAccount = new UserAccount();
         //Extrapolate data from RegisterRequest dto to get the basic info for signup
@@ -24,5 +37,18 @@ public class AuthService {
         userAccount.setCreated(Instant.now());
         //The users account must be enabled befoere it can login, set it to disabled
         userAccount.setEnabled(false);
+        userAccountRepository.save(userAccount);
+
+        String token = generateVerificationToken(userAccount);
+    }
+
+    private String generateVerificationToken(UserAccount userAccount) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(userAccount);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
