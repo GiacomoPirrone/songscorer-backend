a4f7bfb1524886d9c17b6fb81ecca65dc2f4fc70
GiacomoLPirrone <giacomollpirrone@gmail.com>
2021-01-28 21:12:57 +1100

    Implemented a standard password encrypter for user account passwords so that they are not stored as plaintext in the database, this is a simple security measure, I am using bcrypt in this instance

--- src/main/java/com/songscorer/songscorer/service/AuthService.java
+++ src/main/java/com/songscorer/songscorer/service/AuthService.java
@@ -2,6 +2,8 @@ package com.songscorer.songscorer.service;
 
 import com.songscorer.songscorer.dto.RegisterRequest;
 import com.songscorer.songscorer.model.UserAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -9,12 +11,15 @@ import java.time.Instant;
 @Service
 public class AuthService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         UserAccount userAccount = new UserAccount();
         //Extrapolate data from RegisterRequest dto to get the basic info for signup
         userAccount.setUsername(registerRequest.getUsername());
         userAccount.setEmail(registerRequest.getEmail());
-        userAccount.setPassword(registerRequest.getPassword());
+        userAccount.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         //The users account must document when the account was created, take the time of the instant
         userAccount.setCreated(Instant.now());
         //The users account must be enabled befoere it can login, set it to disabled
