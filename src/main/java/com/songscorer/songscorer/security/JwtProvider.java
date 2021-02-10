e65792fde3d7f0c843ba717bd1a1661f0cf37a9d
GiacomoLPirrone <giacomollpirrone@gmail.com>
2021-02-10 19:03:49 +1100

    validation now complete, users must have jwt token associated with them in order to access certain endpoints

--- src/main/java/com/songscorer/songscorer/security/JwtProvider.java
+++ src/main/java/com/songscorer/songscorer/security/JwtProvider.java
@@ -1,6 +1,7 @@
 package com.songscorer.songscorer.security;
 
 import com.songscorer.songscorer.exceptions.SymphonyzeException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,7 @@ import java.security.*;
 import java.security.cert.CertificateException;
 
 import static com.songscorer.songscorer.constants.JksConstants.*;
+import static io.jsonwebtoken.Jwts.parser;
 
 
 @Service
@@ -45,5 +47,28 @@ public class JwtProvider {
         } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
             throw new SymphonyzeException(("Exception occurred while retrieving public key from key store"));
         }
+    }
+
+    public boolean validateToken(String jwt) {
+        parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
+        return true;
+    }
+
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate(JKS_ALIAS).getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new SymphonyzeException("Exception occured while " +
+                    "retrieving public key from the keystore");
+        }
+    }
+
+    public String getUsernameFromJwt(String token){
+        Claims claims = parser()
+                .setSigningKey(getPublicKey())
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 }
